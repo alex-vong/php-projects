ba70adeb4b78834fd451ec7b34a8c2de27eb1279
Alex Vong <avong003@gmail.com>
2021-05-19 11:38:51 -0700

    operators

--- conditionals/index.php
+++ conditionals/index.php
@@ -2,9 +2,17 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
+  <meta charset="UTF-8" />
   <meta name="viewport"
-        content="width=device-width, initial-scale=1.0">
+        content="width=device-width, initial-scale=1.0" />
+  <meta name="description"
+        content="PHP-ractice" />
+  <meta name="og:description"
+        content="PHP Conditional and Boolean and Stuff" />
+  <meta property="og:image"
+        content="[[ Meta-Image Goes Here]]" />
+  <meta property="og:title"
+        content="PH-Practice!" />
   <title>Conditional Practice </title>
   <link rel="stylesheet"
         href="styles.css">
@@ -19,16 +27,14 @@ echo "<h2> Age Requirement</h2>";
 echo "<p>Set a variable to a certain age and made a 'if' conditional that will see if variable is equal to or greater than 21, if it is then it will output 'Have a beer because you are AGE or over! If the condition is not met then it will output 'Have some chocolate milk because you are AGE years old!' </p>";
 
 
-
 $age = 21;
 $weather = 'sunny';
-$hungry = false;
+
 
 if ($age >= 21) {
   echo "<span class='conditional'>" . "Output 1:" . "</span>" . "Have a beer because you are " ."<span class='conditional'>" . $age . "</span>" . " or over!";
 } else {
   echo "<span class='conditional'>" . "Output 2:" . "</span>" . "Have some chocolate milk because you are " ."<span class='conditional'>" . $age . "</span>" . " years old!";
-
 }
 
 echo "<h2> Weather</h2>";
@@ -41,7 +47,7 @@ if ($weather === 'raining') {
 }
 
 echo "<h2> Day of the Week</h2>";
-echo "<p>Checks to see what day the variable is set to, if so will output 'Bring and umbrella because it is RAINING' or else 'You do not need an umbrella because it is NOT raining, in fact, it is VARIABLE! </p>";
+echo "<p>Checks to see what day the variable is set to and depending on the day will output a statement about that variable day</p>";
 $day = 'Tuesday';
 
 if ($day === 'Monday') {
@@ -58,7 +64,24 @@ if ($day === 'Monday') {
   echo "Today is " . "<span class='conditional'>" . $day . "</span>" . "! I love the weekend!";
 } else {
   echo " " . "<span class='conditional'>" . $day . "</span>" . "'s are meant for relaxing!";
+};
+
+echo "<h2> Hungers </h2>";
+echo "<p>Checks to see if HUNGRY and haveMoney is true or false and will output a statement depending on those booleans</p>";
+
+$hungry = true;
+$money = false;
+
+if ($hungry && $money) {
+  echo "<span class='conditional'>" . "Output 1:" . "</span>" . "Go buy In-N-Out!";
+} else if ($hungry && !$money) {
+  echo "<span class='conditional'>" . "Output 2:" . "</span>" . " You're broke so make a sandwhich at home";
+} else if (!$hungry && $money) {
+  echo "<span class='conditional'>" . "Output 3:" . "</span>" . "You're not even hungry, go away!";
+} else {
+  echo "<span class='conditional'>" . "Output 4:" . "</span>" . "Who do I look like, your Mom?";
 }
+
 
 $name = 'Alex';
 $lastName = 'Vong';
