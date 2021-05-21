8b5c3f6ff314e9916df1ef8d043f9cc72a4fa323
Alex Vong <avong003@gmail.com>
2021-05-21 02:29:01 -0700

    svg

--- monster/index.php
+++ monster/index.php
@@ -1,46 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta
-			name="description"
-			content="Adopt Some Cute Monsters Today!"
-		/>
-		<meta
-			name="og:description"
-			content="Adopt Some Cute Monsters Today!"
-		/>
-		<meta
-			property="og:image"
-			content="https://peprojects.dev/alpha-1/alexvong/portraits/monster-meta.png"
-		/>
-		<meta property="og:title" content="Monster Adoption Agency" />
-		<link rel="shortcut icon" type="image/jpg" href="monster-fav.png" />
-
-    <title>Monster Adoption</title>
-		<link
-			rel="stylesheet"
-			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-			integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-			crossorigin="anonymous"
-		/>
-
-		<link rel="stylesheet" href="css/styles.css" />
-</head>
+<?php
+  include ('head.php');
+?>
 
 <body>
- <header class="nav-bar">
-	<div class="inner-column">
-		<picture>
-			<?php include('svg.php');?>
-		</picture>
-		<h1 class="header big-text">Monster Adoption Agency</h1>
-	</div>
-</header>
-
-
   <?php
+
+   include('nav-bar.php');
 
       $codey = [
         "id" => 8,
