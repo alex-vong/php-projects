e254bdb307f6f37f519d20f1187d621fa8928a91
Alex Vong <avong003@gmail.com>
2021-05-26 21:53:59 -0700

    last name

--- forms/index.php
+++ forms/index.php
@@ -11,9 +11,11 @@
 	<h2>Text Input</h2>
 
 	<form action="">
-	<label for="">First Name:</label>
-	<input type="text" placeholder="First Name">
-	<input type="text" placeholder="Last Name">
+	<label for="first-name">First Name:</label>
+	<input type="text" name="first-name" id="first-name" placeholder="First Name">
+
+	<label for="last-name">Last Name:</label>
+	<input type="text" name="last-name" id="last-name" placeholder="Last Name">
 	<input type="email" placeholder="email">
 	<br>
 	<input type="password" placeholder="password">
