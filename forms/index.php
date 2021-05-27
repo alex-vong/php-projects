e2f0154ef081c4f97f409304911c945f17256269
Alex Vong <avong003@gmail.com>
2021-05-26 21:51:43 -0700

    label

--- forms/index.php
+++ forms/index.php
@@ -7,6 +7,25 @@
 	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
+	<h1>Forms</h1>
+	<h2>Text Input</h2>
+
+	<form action="">
+	<label for="">First Name:</label>
+	<input type="text" placeholder="First Name">
+	<input type="text" placeholder="Last Name">
+	<input type="email" placeholder="email">
+	<br>
+	<input type="password" placeholder="password">
+
+
+
+	</form>
+
+
+
+
+	
 	
 </body>
 </html>
