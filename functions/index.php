ebb09027cf1a56980c9e7bc57eb461340357e768
Alex Vong <avong003@gmail.com>
2021-05-27 20:38:50 -0700

    madlib

--- functions/index.php
+++ functions/index.php
@@ -1,16 +1,17 @@
 
 <?php
 
-function add($num1, $num2){
-	echo $num1 + $num2;
-};
+function madlib($noun1, $noun2, $adjective, $noun3) { 
 
-function double($num1) {
-	echo $num1 * 2;
-};
+	echo "<p>" . "On the first day of Christmas, my true love gave to me, a/an $noun1 in a/an $noun2 tree. On the second day of Christmas, my true love gave to me, two $adjective dooves and a/an $noun3 in a pear tree." . "</p>";
 
-add(4, 5);
-// double(50);
+	echo "<p>" . "On the first day of Christmas, my true love gave to me, a/an " . $noun1 . " in a/an " . $noun2 . " tree. On the second day of Christmas, my true love gave to me, two " . $adjective . " dooves and a/an " . $noun3 . " in a pear tree." . "</p>";
+
+ };
+	
+madlib("butt", "cat", "dog", "woof");
+// madlib("car", "tornado", "scraggily", "candle");
+// madlib("butt", "cat", "dog", "woof");
 
 
-?>
+ ?>
