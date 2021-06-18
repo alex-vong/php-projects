6372e111d1fd3c77568453eaa65379de544c0f55
Alex Vong <avong003@gmail.com>
2021-06-18 14:38:58 -0700

    survey

--- madlib/survey.php
+++ madlib/survey.php
@@ -1,19 +1,20 @@
 
 <?php
 
-echo "<h2 class='student-survey'>Student Survey</h2>";
-
 $name = 'Alex';
-$ageInHumanYears = 31;
-$cityResiding = 'Pasadena';
-$stateResiding = 'CA';
-$favoriteColor = 'blue';
-$favoriteFood = 'lobster tacos';
+$age = 31;
+$city = 'Pasadena';
+$state = 'CA';
+$color = 'blue';
+$food = 'lobster tacos';
 $dogName = 'Mika';
 $dogBreed = 'pitbull';
-$ageInDogYears = $ageInHumanYears * 7;
+$ageInDogYears = $age * 7;
 
-
-echo "<p class='student'>" . "Hello, my name is " . $name . " and I am " . $ageInHumanYears . " years old. I live in " . $cityResiding . ", " . $stateResiding . ". My favorite color is " . $favoriteColor . " and I love to eat " . $favoriteFood . ". My dog's name is " . $dogName . " and she is a " . $dogBreed . ". My age in dog years is " . $ageInDogYears . " years old. Woof!" . "</p>";
+echo $ages;
 
 ?>
+
+<h2 class='student-survey'>Student Survey</h2>
+
+<p class="student"> Hello, my name is <?=$name?> and I am <?=$age?> years old. I live in <?=$city?>, <?=$state?>. My favorite color is <?=$color?> and I love to eat <?=$food?>. My dog's name is <?=$dognName?> and she is a <?=$dogBreed?>. My age in dog years is <?=$ageInDogYears?> years old. Woof! </p>
