155b59dbe48f0388ad9ad7b05d7791c8069d32ff
Alex Vong <avong003@gmail.com>
2021-07-29 12:12:44 -0700

    home

---
+++ pokedex/home.php
@@ -0,0 +1,19 @@
+<h1>Pokedex Home Page</h1>
+
+<h2>Search By Region:</h2>
+
+<section class="regions">
+	<inner-column>
+		<div class="region-cards">
+			<div class="kanto">
+	 			<a href='?page=kanto'>Kanto</a>
+			</div>
+			<div class="johto">
+	 			<a href='?page=johto'>Johto</a>
+			</div>
+			<div class="hoenn">
+	 			<a href='?page=hoenn'>Hoenn</a>
+			</div>
+		</div>
+	</inner-column>
+</section>
