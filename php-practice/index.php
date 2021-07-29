364f652d5afa49a31e62391da945550aeada007a
Alex Vong <avong003@gmail.com>
2021-07-29 12:13:11 -0700

    practice

--- php-practice/index.php
+++ php-practice/index.php
@@ -1,110 +1,202 @@
+<?php 
+	$userName = $_GET['age'];
+
+	echo $userName;
+
+
+ ?>
+
+
+
+
+
+
+
+<?php include 'head.php'?>
+
+
+<h4>Create 3 normal arrays</h4>
+
 <?php
 
-include'head.php';
+$groceryList = ['apples', 'oranges', 'bananas', 'diet coke'];
 
-echo "<strong>adding 2 numbers ( 8 + 10 ):</strong>";
-echo "<br>";
-echo 8 + 10;
-echo "<br>";
-echo "<br>";
+$toDoToday = ['daily standup', 'associative arrays lesson', 'php practice', 'url querying'];
 
-echo '<strong>adding 2 strings ( "8" + "10" ):</strong>';
-echo "<br>";
-echo "8" + "10";
-echo "<br>";
-echo "<br>";
+$disneyRides = ['space mountain', 'haunted mansion', 'expedition everest', 'rocking rollercoaster'];
 
-echo '<strong>adding 1 string and 1 number ("eight" + 10 ):</strong>';
-echo "<br>";
-echo "eight" + 10;
-echo "<br>";
-echo "<br>";
-
-echo '<strong>adding 1 string that is a number inside and 1 number ("8" + 10 ): </strong>';
-echo "<br>";
-echo "8" + 10;
-echo "<br>";
-echo "<br>";
-
-echo "<strong>concat 2 numbers ( 8 . 10 ):</strong>";
-echo "<br>";
-echo 8 . 10;
-echo "<br>";
-echo "<br>";
-
-echo '<strong>adding 1 string and 1 number ("8" . 10 ):</strong>';
-echo "<br>";
-echo "8" . 10;
-echo "<br>";
-echo "<br>";
-
-echo "<strong>mult. 2 numbers ( 10 * 10 ):</strong>";
-echo "<br>";
-echo 10 * 10;
-echo "<br>";
-echo "<br>";
-
-echo "<strong>sub. 2 numbers ( 8 - 19 ):</strong>";
-echo "<br>";
-echo 8 - 19;
-echo "<br>";
-echo "<br>";
-
-echo "<strong>divide 2 numbers ( 100 /5 ):</strong>";
-echo "<br>";
-echo 100 /  5;
-echo "<br>";
-echo "<br>";
-
-
-echo "<strong>concat 2 strings:</strong>";
-echo "<br>";
-echo "Alex" . " " . " Vong";
-echo "<br>";
-echo "<br>";
-
-echo "<strong>concat number and string:</strong>";
-echo "<br>";
-echo "Alex" . " " . "32";
-echo "<br>";
-echo "<br>";
-
-$first = 'Alex';
-$last = 'Vong';
-
-$guest = 5;
-$people = $guest + 1;
-$berriesPerBox = 30;
-$pricePerBox = 10.99;
-$totalPriceOfBerries = $people * $pricePerBox;
-
-$total = $guest * $berries;
-
-
-$holiday = 'Christmas';
-$currentDay = date("m/d/Y");
-$currentDayInNumber = 202;
-$christmas = "12/25/2021";
-$christmasDayInNumber = 359;
-$countdown = $christmasDayInNumber - $currentDayInNumber;
-$present = 'software developer job';
-
-
-
+var_dump($groceryList);
+echo '<br><br>';
+var_dump($toDoToday);
+echo '<br><br>';
+var_dump($disneyRides);
 
 ?>
 
-<h3>Create a variable and assign a string to it and print it</h3>
-<p>Hello my name is <?=$first?> <?=$last?></p>
+<h4>echo 2 different items from the normal array</h4>
 
-<h3>Ivy's Guest wants Berries!</h3>
-<p>Ivy is having <strong><?=$guest?></strong> guest over at her lovely home for brunch, which including herself will be a total <strong><?=$people?></strong> people. She decides to serve berries to her guest so shes goes to the local farmers market. Being the generous host that she is she buys each guest an entire box of berries. Each box has <strong><?=$berriesPerBox?></strong>lbs of berries at <strong>$<?=$pricePerBox?></strong> per box. In total she spent <strong>$<?=$totalPriceOfBerries?></strong> for <strong><?=$people?></strong> boxes of berries. How <strong>BERRY</strong> generous of her.</p>
-
-<h3>Use at least 5 variables, one math equation, echo a message that combines the outcome</h3>
-
-<p>My favorite holiday is <strong><?=$holiday?></strong>! Today's date is <strong><?=$currentDay?></strong> and <?=$holiday?> is on <strong><?=$christmas?></strong>. That means there are only <strong><?=$countdown?></strong> days until <?=$holiday?>. I hope that Santa will bring me a <strong><?=$present?></strong>!</p>
+<div><?=$groceryList[0]?></div>
+<div><?=$disneyRides[2]?></div>
 
 
+<h4>Change 2 items in the normal array</h4>
+
+<p>Before with 'apple' at 0</p>
+<?php var_dump($groceryList); ?>
+<p>After with 'kiwi' as 0</p>
+<?php $groceryList[0] = 'kiwis'; 
+var_dump($groceryList);
+?>
+
+<p>Before with 'rockin rollercoaster' at 3</p>
+<?php var_dump($disneyRides); ?>
+<p>After with 'pirates of the carribean' at 3</p>
+<?php $disneyRides[3] = 'Pirates of the Carribean'; 
+var_dump($disneyRides);
+?>
+
+
+<h4>Add some items to normal array</h4>
+
+<p>Adding 'Cheese' to end of array</p>
+<?php 
+array_push($groceryList, 'cheese'); 
+var_dump($groceryList);
+?>
+
+
+<p>Adding 'Big Thunder Railroad' to end of array</p>
+<?php 
+array_push($groceryList, 'Big Thunder Railroad'); 
+var_dump($groceryList);
+?>
+
+
+
+<h4>Create 3 associative arrays</h4>
+
+<?php 
+
+$toyStory = [
+	'filmName' => 'Toy Story',
+	'year' => 1995,
+	'director' => 'John Lasseter',
+	'genre' => 'kids',
+	'gross' => 3630071400
+]; 
+
+$dieHard = [
+	'filmName' => 'Die Hard with a Vengeance',
+	'year' => 1995,
+	'director' => 'John McTiernan',
+	'genre' => 'action-thriller',
+	'gross' => 366101666
+]; 
+
+$apollo13 = [
+	'filmName' => 'Apollo 13',
+	'year' => 1995,
+	'director' => 'Ron Howard',
+	'genre' => 'action',
+	'gross' => 355237933
+];
+
+
+$filmsOf1995 = [$toyStory, $dieHard, $apollo13];
+
+
+
+
+
+$filmsIn1995 = [
+
+	$toyStory = [
+	'filmName' => 'Toy Story',
+	'year' => 1995,
+	'director' => 'John Lasseter',
+	'genre' => 'kids',
+	'gross' => 3630071400
+	],
+
+$dieHard = [
+	'filmName' => 'Die Hard with a Vengeance',
+	'year' => 1995,
+	'director' => 'John McTiernan',
+	'genre' => 'action-thriller',
+	'gross' => 366101666
+	],
+
+$apollo13 = [
+	'filmName' => 'Apollo 13',
+	'year' => 1995,
+	'director' => 'Ron Howard',
+	'genre' => 'action',
+	'gross' => 355237933
+	]
+
+];
+// var_dump($filmsIn1995[0]);
+$filmsIn1995[0]['studio'] = 'pixar';
+var_dump($filmsIn1995[0]);
+
+
+
+
+// var_dump($toyStory);
+// echo '<br><br>';
+// var_dump($dieHard);
+// echo '<br><br>';
+// var_dump($apollo13);
+
+ ?>
+
+
+
+
+
+<!-- 
+<h4>Loop throught associative array</h4>
+
+
+<ul>List all film titles
+ <?php 
+ foreach ($filmsIn1995 as $film) { ?>
+ 	
+ 	<li><?=$film['filmName']?></li>
+ 
+ <?php } ?>
+
+ </ul>
+
+
+
+ <h4>Echo 2 different items from associative array</h4>
+
+<div><?=$toyStory['filmName']?></div>
+<div><?=$apollo13['director']?></div>
+
+
+
+<h4>Remove 3 keys from associaitive Array</h4>
+
+<p>Remove 'gross' from Apollo 13</p>
+<?php unset($apollo13['gross']);
+var_dump($apollo13)?>
+
+
+<p>Remove 'director' from Die Hard</p>
+<?php unset($dieHard['director']);
+var_dump($dieHard)?>
+
+<h4>Add 2 keys to associative array:</h4>
+
+<p>Add 'studio' to Toy Story</p>
+<?php $toyStory['studio'] = 'Pixar';
+var_dump($toyStory)?>
+
+<p>Add 'Starring' to Die Hard</p>
+<?php $dieHard['starring'] = 'Bruce Willis';
+var_dump($dieHard)?>
 
 
 
@@ -121,6 +213,4 @@ $present = 'software developer job';
 
 
 
-
-
-
+ -->
